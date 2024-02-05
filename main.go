fa73ac4b830ecfa3f0a38c8f4cf15e9ae704d05c
feelonewong <feelonewong@gmail.com>
2024-02-05 17:58:12 +0800

    update: 添加gin

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "blog-server/database"
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func Home(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"Message": "pong",
+	})
+}
+
+func Html(c *gin.Context) {
+	c.HTML(200, "login.html", nil)
+}
 func main() {
-	database.GetBlogDBConnection()
+	r := gin.Default()
+	r.LoadHTMLFiles("views/login.html")
+	r.GET("/", Home)
+	r.GET("/login", Html)
+	r.Run("127.0.0.1:5678")
 }
